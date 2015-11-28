d36e0630ffc4d8abf1277ead0b0141e2d7df8baa
Anton Kalenik <kalenik.online@gmail.com>
2015-11-28 20:51:12 +0200

    Строка в формате nowdock

---
+++ 01-Strings/1.5.1.php
@@ -0,0 +1,14 @@
+<?php
+$js = <<< '__JS__'
+$.ajax({
+    'url': '/api/getStock',
+    'data': {
+        'ticker': 'LNKD'
+    },
+    'success': function(data) {
+        $("#stock-price").html("<strong>$" + data + "</strong>");
+    }
+});
+__JS__;
+
+print $js;
