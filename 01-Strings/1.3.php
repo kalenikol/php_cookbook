e9fcc733302656218cdf283e193150079ee32667
Anton Kalenik <kalenik.online@gmail.com>
2015-11-28 20:26:33 +0200

    Дополнительный пример строк в формате heredoc

---
+++ 01-Strings/1.3.php
@@ -0,0 +1,13 @@
+<?php
+print <<< PARSLEY
+It's easy to grow fresh<br>
+Parsley<br>
+Chives<br>
+on your windowsill
+PARSLEY;
+
+print <<< DOGS
+If you like pets, yell out:<br>
+DOGS AND CATS ARE GREAT
+DOGS;
+
