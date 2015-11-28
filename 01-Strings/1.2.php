c538e53b33d6ee217da7e68aef75ee8c0f17865c
Anton Kalenik <kalenik.online@gmail.com>
2015-11-28 20:20:11 +0200

    Определение строки в формате heredoc

---
+++ 01-Strings/1.2.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 28.11.2015
+ * Time: 20:16
+ * heredoc
+ */
+print <<< END
+It's funny when signs say things like:<br>
+    Original "Root" Beer<br>
+    "Free gift"<br>
+    Shoes cleaned while you "wait"<br>
+or have other misquoted words.
+END;
