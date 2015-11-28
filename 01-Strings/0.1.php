a969c18fd61717fc0803afe63d3e992be92c22c9
Anton Kalenik <kalenik.online@gmail.com>
2015-11-28 20:05:48 +0200

    0.1, 1.1

---
+++ 01-Strings/0.1.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 28.11.2015
+ * Time: 19:52
+ * Одинарные кавычки
+ */
+print 'I have gone to the store.<br>';
+print 'I\'ve gone to the store.<br>';
+print 'Would you pay $1.75 for 8 ounces of tap water?<br>';
+print 'In double-quoted strings, newline is represented by \n<br>';
---
+++ 01-Strings/1.1.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 28.11.2015
+ * Time: 20:00
+ * Двойные кавычки
+ */
+print "I've gone to the store.<br>";
+print "The sauce cost \$10.25<br>";
+$cost = '$10.25';
+print "The sauce cost $cost<br>";
+print "The sauce cost \$\061\060.\x32\x35<br>";
