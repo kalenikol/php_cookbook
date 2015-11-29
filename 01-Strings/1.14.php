7c4311e20e3d1c13d036a1cc609f505ce9b56051
Anton Kalenik <kalenik.online@gmail.com>
2015-11-29 14:15:26 +0200

    Замена подстроки с использованием substr_replace

---
+++ 01-Strings/1.14.php
@@ -0,0 +1,9 @@
+<?php
+//$new_string = substr_replace($old_string, $new_substring, $start);
+//$new_string = substr_replace($old_string, $new_substring, $start, $length);
+
+print substr_replace('My pet is a blue dog.', 'fish.', 12);
+print substr_replace('My pet is a blue dog.', 'green', 12,4);
+$credit_card = '4111 1111 1111 1111';
+print substr_replace($credit_card, 'xxxx', 0, strlen($credit_card) - 4);
+print substr_replace('My pet is a blue dog.', 'Title: ', 0, 0);
